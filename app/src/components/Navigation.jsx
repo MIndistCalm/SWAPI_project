d8da17b6f44693f3b22db50ef8f94036d3caba97
MIndistCalm <suxinin5@yandex.ru>
2023-01-20 17:25:12 +0700

    Added Navigation bar

--- app/src/components/Navigation.jsx
+++ app/src/components/Navigation.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Navigation = () => {
   return (
-    <nav className='flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white'>
-      <h3 className='font-bold'>Star Wars</h3>
+    <nav className='flex justify-between items-center h-[60px] px-5 shadow-md bg-gray-500 text-white'>
+      <h3 className='font-bold text-xl'>Star Wars</h3>
 
       <span>
-        <Link to='/' className='mr-2'>Home</Link>
-        <Link to='/favourites'>Favourites</Link>
+        <Link to='/' className='mr-2 text-lg'>Home</Link>
+        <Link to='/favourites' className='text-lg'>Favourites</Link>
 
-        
+
       </span>
     </nav>
   )
