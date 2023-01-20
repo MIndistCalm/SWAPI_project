9bcf3e7902a4aaf518e1f8aaa7295038da97fdb4
MIndistCalm <suxinin5@yandex.ru>
2023-01-20 17:26:44 +0700

    Modified redux store

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/dist/query'
 import { peopleApi } from './peoples/people.api'
+import { peopleReducer } from './peoples/people.slice'
 
 const store = configureStore({
   reducer: {
     [peopleApi.reducerPath]: peopleApi.reducer,
+    favourites: peopleReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(peopleApi.middleware),
 })
